1ab7d4d70a803b8aca05a5032c83ebaaefb3705e
TanyaHoncharova <tanushaGK@gmail.com>
2021-07-29 12:04:04 +0300

    fix scrolling

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Button extends Component {
-    componentDidMount() {
+    componentDidUpdate() {
         window.scrollTo({
           top: document.documentElement.scrollHeight,
           behavior: 'smooth',
